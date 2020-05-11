377662eab644cb98b00f650871e181bfa392a744
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-11 15:13:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,10 +6,10 @@ class Dog
     @name
   end 
   
-  def breed=(breed)
-    @dog_breed = breed
+  def breed=(dog_breed)
+    @breed = dog_breed
   end 
   def breed 
-    @dog_breed
+    @breed
   end 
 end 
